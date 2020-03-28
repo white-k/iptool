9d6ab18572272beaf51c798249d44a989ceec1cb
white-k <user-zjiong@outlook.com>
2020-03-28 22:22:34 +0800

    Update te.c

--- te.c
+++ te.c
@@ -1,9 +1,9 @@
-
+//获取主机ip地址：gethostbyname
 #include<sys/socket.h>
-#include<netdb.h>
+#include<netdb.h>  //gethostbyname 函数头文件 函数返回struct hostent类型的指针
 #include<stdio.h>
-#include<stdlib.h>
-#include<arpa/inet.h>
+#include <stdlib.h>
+#include <arpa / inet.h> //inet_ntop 函数
 int main(int argc,char *argv[])
 {
 	struct sockaddr_in localaddr;
@@ -18,14 +18,14 @@ int main(int argc,char *argv[])
 
 	host=argv[1];
 	ptr=gethostbyname(host);
-	for(paddr=ptr->h_aliases;*paddr!=NULL;paddr++)
+	for(paddr=ptr->h_aliases;*paddr!=NULL;paddr++)  
 		printf("\talias:%s\n",*paddr);
 	switch(ptr->h_addrtype)
 	{
 		case AF_INET:
 			paddr=ptr->h_addr_list;
 			for(;*paddr!=NULL;paddr++)
-				printf("%s ip is %s",host,inet_ntop(AF_INET,*paddr,strbuf,sizeof(strbuf)));
+				printf("%s ip is %s\n",host,inet_ntop(AF_INET,*paddr,strbuf,sizeof(strbuf)));
 		
 		break;
 	}
